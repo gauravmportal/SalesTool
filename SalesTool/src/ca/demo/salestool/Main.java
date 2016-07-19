b7ef17c9cd2741272e83216fe8ce7a7c3c24d6e7
gaurav.singh <gaurav.mportal@gmail.com>
2016-07-19 23:58:12 +0530

    Initial commit sales tool

---
+++ SalesTool/src/ca/demo/salestool/Main.java
@@ -0,0 +1,18 @@
+package ca.demo.salestool;
+
+public class Main {
+	public static void main(String[] args)
+	{
+		SalesData data = new SalesData();
+		displayGreetings();
+		data.display();
+		
+	}
+	
+	public static void displayGreetings()
+	{
+		System.out.println("Hello Happy Sales people!");
+		System.out.println("This App shows sales data");
+	}
+
+}
